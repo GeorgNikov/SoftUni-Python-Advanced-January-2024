9491a1c97631eb4f4f434a9e56a4cc9503265db5
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2024-01-10 21:50:39 +0200

    Update 9.01.2024

---
+++ Exercise - Lists as Stacks and Queues/02. Stacked Queries.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+stack = []
+
+for _ in range(n):
+    token = input().split()
+    if len(token) == 2:
+        _, number = token
+        stack.append(int(number))
+    elif len(token) == 1:
+        number = int(token[0])
+        if number == 2 and stack:
+            stack.pop()
+        elif number == 3:
+            print(max(stack))
+        elif number == 4:
+            print(min(stack))
+
+if stack:
+    while stack:
+        if len(stack) > 1:
+            print(stack.pop(), end=', ')
+        else:
+            print(stack.pop())
---
+++ Exercise - Lists as Stacks and Queues/1. Reverse Numbers.py
@@ -0,0 +1,4 @@
+numbers = list(input().split())
+
+for i in range(len(numbers)):
+    print(numbers.pop(), end=' ')
